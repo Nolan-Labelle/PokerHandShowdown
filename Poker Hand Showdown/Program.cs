b24d3c452699803ea6745fe9f15e213b3f6ce02f
Nolan-Labelle <nolanhlabelle@gmail.com>
2018-08-09 22:42:09 -0500

    Added Regex parsing on input, and creation of Cards

--- Poker Hand Showdown/Program.cs
+++ Poker Hand Showdown/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using PokerLibrary;
 
@@ -14,18 +15,20 @@ namespace Poker_Hand_Showdown
             string tempName;
             string tempCardString;
             Card tempCard;
+            Hand tempHand = new Hand();
+
+            Regex Regex = new Regex(@"([2-9]|10|J|Q|K|A)(S|H|D|C)");
+            Match match;
 
 
             Console.Write("Player 1's Name: ");
             tempName = Console.ReadLine();
 
-            Console.WriteLine("Please input {0}s Hand usaing a value from 2-10, or J, Q, K, or A, and a Suit, 'S'pades, 'H'earts, 'D'iamonds, 'C'lubs.", tempName);
-            do
-            {
-                Console.Write("Card 1 Value: ");
-                tempCardString = Console.ReadLine();
-                
-            } while ();
+            Console.WriteLine("Please input {0}s Hand of '5' cards with the format #$, where the # is a value from 2-10, or J, Q, K, or A, and the $ is the Suit, 'S'pades, 'H'earts, 'D'iamonds, 'C'lubs.", tempName);
+            tempCardString = Console.ReadLine();
+
+            match = Regex.Match(tempCardString.ToUpper());
+
         }
     }
 }
--- PokerLibrary/PokerLibrary.cs
+++ PokerLibrary/PokerLibrary.cs
@@ -3,73 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace PokerLibrary
 {
-    enum Deck
-    {
-        Card2H,
-        Card3H,
-        Card4H,
-        Card5H,
-        Card6H,
-        Card7H,
-        Card8H,
-        Card9H,
-        Card10H,
-        CardJH,
-        CardQH,
-        CardKH,
-        CardAH,
-        Card2C,
-        Card3C,
-        Card4C,
-        Card5C,
-        Card6C,
-        Card7C,
-        Card8C,
-        Card9C,
-        Card10C,
-        CardJC,
-        CardQC,
-        CardKC,
-        CardAC,
-        Card2D,
-        Card3D,
-        Card4D,
-        Card5D,
-        Card6D,
-        Card7D,
-        Card8D,
-        Card9D,
-        Card10D,
-        CardJD,
-        CardQD,
-        CardKD,
-        CardAD,
-        Card2S,
-        Card3S,
-        Card4S,
-        Card5S,
-        Card6S,
-        Card7S,
-        Card8S,
-        Card9S,
-        Card10S,
-        CardJS,
-        CardQS,
-        CardKS,
-        CardAS,
-        END_OF_DECK
-    }
     public struct Card
     {
-        private int Value { get; set; }
-        private char Suit { get; set; }
-        public Card(int value, char suit)
+        private string Value { get; set; }
+        private string Suit { get; set; }
+
+        public Card(string input)
         {
-            Value = value;
-            Suit = suit;
+            Regex valueRegex = new Regex(@"[2-9]|10|J|Q|K|A");
+            Regex suitRegex = new Regex(@"S|H|D|C");
+            Match valueMatch = valueRegex.Match(input);
+            Match suitMatch = suitRegex.Match(input);
+
+            if (valueMatch.Success && suitMatch.Success)
+            {
+                Value = valueMatch.Groups[1].Value;
+                Suit = suitMatch.Groups[1].Value;
+                Console.WriteLine("Successfully parsed {0} into Value {1} and Suit {2}", input, Value, Suit);
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse {0}", input);
+                Value = null;
+                Suit = null;
+            }
         }
     }
 
@@ -77,10 +38,12 @@ namespace PokerLibrary
     {
         private Card[] Cards { get; set; }
         private int CardCount { get; set; }
+
         public Hand()
         {
             CardCount = 0;
         }
+
         public Hand(Card card1, Card card2, Card card3, Card card4, Card card5)
         {
             Cards[0] = card1;
@@ -90,6 +53,7 @@ namespace PokerLibrary
             Cards[4] = card5;
             CardCount = 5;
         }
+
         public bool insertCard(Card card)
         {
             bool passed = true;
@@ -110,6 +74,7 @@ namespace PokerLibrary
     {
         public string Name { get; set; }
         public Hand Hand { get; set; }
+
         public Player(string name)
         {
             Name = name;
